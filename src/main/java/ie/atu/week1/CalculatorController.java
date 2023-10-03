00bf731f102780c548a286b6b8a21b6f0fe22cbf
G00394277 <G00394277@atu.ie>
2023-10-03 21:29:30 +0100

    PathVariable Calculator

--- src/main/java/ie/atu/week1/CalculatorController.java
+++ src/main/java/ie/atu/week1/CalculatorController.java
@@ -1,14 +1,15 @@
 package ie.atu.week1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class CalculatorController {
-    @GetMapping("/calculate")
-    public String calculate(@RequestParam int num1, int num2, String operation) {
+    @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public String calculate(@PathVariable int num1, @PathVariable int num2, @PathVariable String operation) {
         int total = 0;
         switch(operation) {
             case "add":
